cdccfee1576d1a8e890d104f6be388c67206acb8
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-08 12:32:31 +0100

    task done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-for i in range(ord("a"), ord("z") + 1):
-    if i != ord("q") and i != ord("e"):
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i != 101 and i != 113:
         print(chr(i), end="")
