f26a9d2acec54341b24014c76e41597535d6d56f
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-10 20:05:10 +0100

    task done

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+adding = add(10, 5)
+subtract = sub(10, 5)
+multiply = mul(10, 5)
+divide = div(10, 5)
+
+print("10 + 5 = {}".format(adding))
+print("10 - 5 = {}".format(subtract))
+print("10 * 5 = {}".format(multiply))
+print("10 / 5 = {}".format(divide))
