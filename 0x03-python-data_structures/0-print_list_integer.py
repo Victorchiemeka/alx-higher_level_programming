62649e9417f1f9e98bfdd9d3b741775d5da6a148
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-12 13:22:07 +0100

    task done

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=None):
-    if my_list is None:
-        my_list = []  # Create a new list if None is passed
-
+def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
