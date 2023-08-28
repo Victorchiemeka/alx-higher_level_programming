0030a50a5a1c8b2ba2ed516ac09ca540321cb7dd
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-28 19:55:51 +0100

    testing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in my_list:
+            print("{:d}".format(i))
+    except IndexError:
+        return False
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
