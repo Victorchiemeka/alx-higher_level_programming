9731d3357f1ca484031b9e9b97c4f81c72b1eb4c
victorchiemeka <victorchukwuemeka127@gmail.com>
2023-10-17 09:50:17 +0100

    deleted unused files

--- 7-update_dictionary.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def update_dictionary(a_dictionary, key, value):
-    # Update or add the key/value pair in the dictionary
-    a_dictionary[key] = value
-
-    return a_dictionary
